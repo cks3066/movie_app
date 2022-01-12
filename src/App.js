4c6d9c113019d4ece6ff815b0e5db1f95c46c53b
DESKTOP-GFIRMRS\HOUSE <cks3066@naver.com>
2022-01-13 03:50:19 +0900

    chore: To Do List Part One

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
 import "./App.css";
 
-const Hello = () => {
-  React.useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-};
-
 const App = (props) => {
-  const [showing, setShowing] = React.useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = React.useState("");
+  const [toDos, setToDos] = React.useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+    setToDos([...toDos, toDo]);
+    setToDos((currentArray) => [...toDos, toDo]);
+    console.log(toDos);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 };

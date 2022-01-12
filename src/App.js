9d6bfde7363350d5f9f33b37cd2ada690acd7fcc
cks3066 <cks3066@naver.com>
2022-01-12 18:32:10 +0900

    chore: useEffect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from "react";
 import "./App.css";
 
 const App = (props) => {
-  const [hours, setHours] = React.useState(2);
-
+  const [counter, setCounter] = React.useState(0);
+  const [keyword, setKeyword] = React.useState("");
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run always");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  React.useEffect(iRunOnlyOnce, []);
+  React.useEffect(() => {
+    console.log("Search For");
+  }, [keyword]);
   return (
-    <>
-      <h1>{hours}</h1>
-    </>
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
   );
 };
 

a0960f6e5db8a2d4ef7d329d50a66a3c5a207ebc
cks3066 <cks3066@naver.com>
2022-01-12 16:59:44 +0900

    chore: 2021ver 초기화

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
-import About from "./routes/About";
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
-import Navigation from "./components/Navigation";
 import "./App.css";
 
 const App = (props) => {
+  const [hours, setHours] = React.useState(2);
+
   return (
-    <HashRouter>
-      <Navigation />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
-    </HashRouter>
+    <>
+      <h1>{hours}</h1>
+    </>
   );
 };
 
